818143000ff8c81b63c6654590d3640c3302a1f0
Alexis Guzman <alexisguzman157@outlook.com>
2021-10-26 16:53:59 -0400

    troubleshooting

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,6 @@
 const FILES_TO_CACHE = [
   "/",
+  "./indexdb.js",
   "./index.html",
   "./icons/icon-192x192.png",
   "./icons/icon-512x512.png",
@@ -18,80 +19,85 @@ self.addEventListener("install", event => {
     caches
       .open(STATIC_CACHE)
       .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
+      //.then(() => self.skipWaiting())
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", event => {
-  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
-  event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        // return array of cache names that are old to delete
-        return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
-        );
-      })
-      .then(cachesToDelete => {
-        return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
+// // The activate handler takes care of cleaning up old caches.
+// self.addEventListener("activate", event => {
+//   const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+//   event.waitUntil(
+//     caches
+//       .keys()
+//       .then(cacheNames => {
+//         // return array of cache names that are old to delete
+//         return cacheNames.filter(
+//           cacheName => !currentCaches.includes(cacheName)
+//         );
+//       })
+//       .then(cachesToDelete => {
+//         return Promise.all(
+//           cachesToDelete.map(cacheToDelete => {
+//             return caches.delete(cacheToDelete);
+//           })
+//         );
+//       })
+//       .then(() => self.clients.claim())
+//   );
+// });
 
-// self.addEventListener('fetch', (event) => {
-  // if (event.request.url.includes('/api/')) {
-  //   event.respondWith(
-  //     caches.open(RUNTIME_CACHE).then(cache => {
-  //       return fetch(event.request).then(response => {
-  //         return cache.put(event.request, response.clone());
-  //         return response;
-  //       })
-  //         .catch(() => caches.match(event.request));
-  //     })
-  //   );
-  //   return;
-  // }
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request.url).then(response => {
+          if(response.status == 200){
+            cache.put(event.request.url, response.clone());
 
-  // use cache first for all other requests for performance
-  // event.respondWith(
-  //   caches.match(event.request).then(cachedResponse => {
-  //     if (cachedResponse) {
-  //       return cachedResponse;
-  //     }
-
-  //     // request is not in cache. make network request and cache the response
-  //     return caches.open(RUNTIME_CACHE).then(cache => {
-  //       return fetch(event.request).then(response => {
-  //         return cache.put(event.request, response.clone()).then(() => {
-  //           return response;
-  //         });
-  //       });
-  //     });
-  //   })
-  // );
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
           }
-  
-          return caches.open(RUNTIME_CACHE).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
+          return response;
         })
-      );
-    }
-  });
+          .catch((err) => {
+            return   cache.match(event.request)
+          
+          });
+      })
+      .catch(err => console.log(err))
+    );
+    return;
+  }
+
+  //use cache first for all other requests for performance
+  event.respondWith(
+    fetch(event.request).catch(function(){
+      return caches.match(event.request).then(function(response){
+        if(response){
+          return response
+        }else if(event.request.headers.get("accept").includes("text/html")){
+          return catches.match("/");
+        }
+      })
+    })
+    
+  );
+
+})
+  // self.addEventListener('fetch', (event) => {
+  //   if (event.request.url.startsWith(self.location.origin)) {
+  //     event.respondWith(
+  //       caches.match(event.request).then((cachedResponse) => {
+  //         if (cachedResponse) {
+  //           return cachedResponse;
+  //         }
+  
+  //         return caches.open(RUNTIME_CACHE).then((cache) => {
+  //           return fetch(event.request).then((response) => {
+  //             return cache.put(event.request, response.clone()).then(() => {
+  //               return response;
+  //             });
+  //           });
+  //         });
+  //       })
+  //     );
+  //   }
+  // });
